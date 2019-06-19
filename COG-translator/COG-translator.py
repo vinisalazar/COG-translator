3e07c770cbf8020d804a1bc46d07e47a56f883ac
Vini Salazar <viniws@gmail.com>
2019-06-19 17:50:32 -0300

    First commit

---
+++ COG-translator/COG-translator.py
@@ -0,0 +1,6 @@
+class COGTranslator(object):
+    """docstring for COGTranslator."""
+
+    def __init__(self, translation_tab):
+        super(COGTranslator, self).__init__()
+        self.translation_tab = "../data/cognames2003-2014.tab"
