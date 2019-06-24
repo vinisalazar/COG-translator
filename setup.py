751e1533fc5fdf36218755e4ef939f3ee892115d
Vini Salazar <viniws@gmail.com>
2019-06-24 15:16:59 -0300

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="COG-translator",
+    version="0.0.1",
+    author="Vini Salazar",
+    author_email="viniws@gmail.com",
+    description="COG-translator, a pure Python translator for COG names.",
+    packages=setuptools.find_packages(),
+)
