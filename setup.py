ad6ed00aad768d1db1c92e30d090b3aa6b6e53f9
Vini Salazar <viniws@gmail.com>
2020-05-13 16:52:27 -0300

    Bump version number in setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="COG-translator",
-    version="0.0.1",
+    version="0.0.2",
     author="Vini Salazar",
     author_email="viniws@gmail.com",
     description="COG-translator, a pure Python translator for COG names.",
